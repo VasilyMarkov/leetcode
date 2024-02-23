630c794e54af32db3ba30a14cb180a3dac357d9b
Vasily Markov <mvv1993Vasya@gmail.com>
2024-02-23 17:19:00 +0300

    isSameTree

---
+++ easy/isSameTree.hpp
@@ -0,0 +1,8 @@
+#include "../binTree.hpp"
+#include <stack>
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(p == nullptr && q == nullptr) return true;
+    if (p == nullptr || q == nullptr || p->val != q->val) return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
